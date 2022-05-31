c8d9dcca35ad9a19b38277f87300ba45c4a9903f
kerim <kerim.qafarli02@gmail.com>
2022-05-31 15:52:23 +0400

    main

--- portfolio/training/index.php
+++ portfolio/training/index.php
@@ -1,3 +1,7 @@
 <?php
 
-phpinfo();
+$connect = mysqli_connect(host:'localhost', user:'kera_kera', password:'5813910K', database:'addressbook');
+
+if (!connect) {
+    echo 'Error connect to database!';
+}
