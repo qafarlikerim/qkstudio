0a4b058fde7aff315ce6c808737479afc60498c1
kerim <kerim.qafarli02@gmail.com>
2022-05-31 17:52:35 +0400

    main

--- portfolio/training/add.php
+++ portfolio/training/add.php
@@ -8,6 +8,36 @@
     exit();
   }
 
+  $price = $_POST['price'];
+  if($price == '') {
+    echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
+    echo 'Введите номер контакта...';
+    exit();
+  }
+
+  $description = $_POST['description'];
+  if($description == '') {
+    echo('<div style="font-size: 30; font-weight: bold;">'.$rez['nazv']);
+    echo 'Введите дату...';
+    exit();
+  }
+
+  require_once 'configDB.php';
+
+  $sql = 'INSERT INTO products(title) VALUES(:title)';
+  $query = $pdo->prepare($sql);
+  $query->execute(['title' => $title]);
+
+  header('Location: /');
+
+  require_once 'configDB.php';
+ 
+  $sql = 'INSERT INTO products(price) VALUES(:price)';
+  $query = $pdo->prepare($sql);
+  $query->execute(['price' => $price]);
+
+  header('Location: /');
+
   require_once 'configDB.php';
  
   $sql = 'INSERT INTO products(description) VALUES(:description)';
