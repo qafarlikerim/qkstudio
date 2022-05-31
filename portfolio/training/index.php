07e51e933b86bc785c68ec249b934eb0937eb7bd
kerim <kerim.qafarli02@gmail.com>
2022-05-31 16:22:49 +0400

    main

--- portfolio/training/index.php
+++ portfolio/training/index.php
@@ -3,6 +3,6 @@
 $connect = @mysqli_connect('localhost', 'kera_kera', '5813910K', 'addressbook');
 
 if (!$connect) {
-    error_log('Ошибка соединения: ' . mysqli_connect_errno());
+    error_log('Ошибка соединения: ' . mysqli_connect_error());
 }
 ?>
