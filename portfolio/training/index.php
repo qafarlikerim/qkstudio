05696f7aa3cbbfb60fe7b5c4eb314c3be4adb2a2
kerim <kerim.qafarli02@gmail.com>
2022-05-31 17:47:33 +0400

    main

--- portfolio/training/index.php
+++ portfolio/training/index.php
@@ -97,11 +97,7 @@ require_once 'connect.php';
     <div class="container">
       <h1>Список контактов</h1>
       <form action="/add.php" method="post">
-        <input type="text" name="title" id="title" placeholder="Введите титл..." class="form-control">
-        <br>
         <input type="text" name="description" id="description" placeholder="Введите описание..." class="form-control">
-        <br>
-        <input type="int" name="price" id="price" placeholder="Введите цену..." class="form-control">
         <br>
         <button type="submit" name="sendTask" class="button button2">+</button>&nbsp;<button type="reset" name="sendTask" class="button button3">-</button>
       </form>  
