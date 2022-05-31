7957b18b35337123677fd20307ab489808d573d8
kerim <kerim.qafarli02@gmail.com>
2022-05-31 16:42:32 +0400

    main

--- portfolio/training/index.php
+++ portfolio/training/index.php
@@ -5,6 +5,6 @@ mysqli_report(MYSQLI_REPORT_OFF);
 $connect = mysqli_connect('localhost', 'kera_kera', '5813910K', 'kera_addressbook');
 
 if (!$connect) {
-    echo 'Error connect to database!';
+    die('Error connect to database!');
 }
 ?>
