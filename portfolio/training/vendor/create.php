7da039cfe179a212e3ac54ab4b331c3593c9ed5c
kerim <kerim.qafarli02@gmail.com>
2022-06-01 16:31:41 +0400

    a

--- portfolio/training/vendor/create.php
+++ portfolio/training/vendor/create.php
@@ -2,4 +2,8 @@
 
 require_once '../config/connect.php';
 
-print_r($_POST);
+$title = $_POST['title'];
+$description = $_POST['description'];
+$price = $_POST['price'];
+
+mysqli_query($connect, query:"INSERT INTO `products` (`id`, `title`, `price`, `descripsion`) VALUES (NULL, '$title', '$description', '$price')")
