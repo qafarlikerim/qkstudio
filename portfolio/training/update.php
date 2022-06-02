824e1b21b1338d424d466bf0ee3c35e1610e2d13
kerim <kerim.qafarli02@gmail.com>
2022-06-02 15:49:33 +0400

    a

--- portfolio/training/update.php
+++ portfolio/training/update.php
@@ -2,7 +2,6 @@
 
     require_once 'config/connect.php';
 
-    $product_id = $_GET['id'];
     $result = $mysqli->query($query);
     while ($row = $result->fetch_assoc()) {
     printf("%s (%s)\n", $row["title"], $row["description"], $row["price"]);
