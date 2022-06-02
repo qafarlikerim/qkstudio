0a4b545715ec616f3d1774dfc8a43ecd44fffb57
kerim <kerim.qafarli02@gmail.com>
2022-06-02 15:53:34 +0400

    a

--- portfolio/training/config/connect.php
+++ portfolio/training/config/connect.php
@@ -7,4 +7,6 @@ $connect = mysqli_connect('localhost', 'kera_kera', '5813910K', 'kera_addressboo
 if (!$connect) {
     die('Error connect to database!');
 }
+
+public mysqli_result::fetch_assoc(): array|null|false
 ?>
