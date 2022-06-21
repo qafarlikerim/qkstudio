cd874f4485c903aa0cc5bf083ed8cfae36db4e30
kerim <kerim.qafarli02@gmail.com>
2022-06-21 14:52:40 +0400

    kjdf

---
+++ portfolio/training/mysql-warrior/main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mysql2 = require('mysql2/promise');
+
+const pool = mysql2.createPool({
+    host: 'localhost',
+    user: 'kera_kera',
+    database: 'kera_addressbook',
+    password: '5813910K',
+});
+
+const app = express();
+
+app.get('/', function(req, res) {
+    pool.query('SELECT * FROM abonents').then(function(data) {
+        const abonents = data[0];
+        res.send(`<!DOCTYPE html>
+        <html>
+            <body>
+                <ul>
+                    ${abonents.map(abonent => `<li>${abonent.name}</li>`).join('')}
+                </ul>
+            </body>
+        </html>`);
+    });
+});
+
+app.listen(3000, function() {
+    console.log('server started!');
+});
